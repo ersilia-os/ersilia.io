4b458285a07121d2f52fa2688bffe6294466a625
miquelduranfrigola <miquelduranfrigola@gmail.com>
2021-01-27 15:46:09 +0100

    copyright added

--- src/layouts/partials/Footer.vue
+++ src/layouts/partials/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="text-center">
-    Proudly developed by
+    Copyright 2020 by
     <g-link to="http://ersilia.io"> Ersilia Open Source Initiative</g-link>
-    <g-link to="https://creativecommons.org/licenses/by/4.0/" style="margin-left: .5rem">Creative Commons By Attribution 4.0 International license</g-link>
+    Content on this website licensed under <g-link to="https://creativecommons.org/licenses/by/4.0/" style="margin-left: .5rem">CC BY 4.0</g-link>
 
   </footer>
 </template>
